fb862adc057cedc1b559b0f9450e70e3aa435ca2
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-04-06 15:56:38 +0100

    project set up

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('renders the app', () => {
+  const { asFragment } = render(<App/>);
+  
+  expect(asFragment()).toMatchSnapshot(); 
+});
