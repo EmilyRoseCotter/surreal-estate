026b1e0a8a9feef6765deddd87063f64c08a66dc
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-04-15 13:46:10 +0100

    started property card

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,9 +1,19 @@
 import React from "react";
+import PropertyCard from "./PropertyCard";
 
 function Properties() {
   return (
     <div>
       <h2>Properties</h2>
+      <PropertyCard
+        title="2 bed cottage"
+        type="Cottage"
+        bedrooms="2"
+        bathrooms="2"
+        city="Manchester"
+        price="£100000"
+        email="estate@estate.com"
+      />
     </div>
   );
 }
---
+++ src/components/PropertyCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/PropertyCard.css";
+
+function PropertyCard({
+  title,
+  type,
+  bathrooms,
+  bedrooms,
+  price,
+  city,
+  email,
+}) {
+  return (
+    <div className="property-card">
+      <div className="property-card-image">
+        <img
+          src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
+          alt="surreal estate logo"
+        />
+      </div>
+      <div className="property-card-title property-card-item">{title}</div>
+      <div className="property-card-type-city property-card-item">
+        {type} - {city}
+      </div>
+      <div className="property-card-bathrooms property-card-item">
+        Bathrooms: {bathrooms}
+      </div>
+      <div className="property-card-bedrooms property-card-item">
+        Bedrooms: {bedrooms}
+      </div>
+      <div className="property-card-price property-card-item">{price}</div>
+      <div className="property-card-email property-card-item">
+        <a className="link-text" href={`mailto:${email}?subject=${title}`}>
+          Email
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default PropertyCard;
+
+PropertyCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  bathrooms: PropTypes.number.isRequired,
+  bedrooms: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
