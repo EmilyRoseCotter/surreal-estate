e319e80869d560334b30edf09a915291c855302f
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-04-16 16:53:28 +0100

    added sidebar

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Alert from "./Alert";
 import PropertyCard from "./PropertyCard";
+import SideBar from "./SideBar";
 import "../styles/Properties.css";
 
 function Properties() {
   const [properties, setProperties] = useState([]);
   const [alert, setAlert] = useState({ message: "" });
+  const { search } = useLocation();
 
   useEffect(() => {
     axios
@@ -21,8 +24,18 @@ function Properties() {
       );
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/api/v1/PropertyListing${search}`)
+      .then(({ data }) => setProperties(data))
+      .catch((err) => console.error(err));
+  }, [search]);
+
   return (
     <div className="properties-page">
+      <div>
+        <SideBar />
+      </div>
       <div className="alert">
         <Alert message={alert.message} success={false} />
       </div>
