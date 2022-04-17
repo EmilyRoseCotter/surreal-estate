e005697d756adcbf5000e9fdcd73b54c783bfefc
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-04-17 13:38:13 +0100

    added sorting by price

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import qs from "qs";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/SideBar.css";
+
+const buildQueryString = (operation, valueObj) => {
+  const { search } = useLocation();
+
+  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+  const newQueryParams = {
+    ...currentQueryParams,
+    [operation]: JSON.stringify(valueObj),
+  };
+  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+};
 
 function SideBar() {
   return (
     <div className="side-bar">
       <p className="filter-title">Filter by city:</p>
       <div className="sidebar-links">
-        <Link className="link-item" to={`/?query={"city":"Manchester"}`}>
+        <Link
+          className="link-item"
+          to={buildQueryString("query", { city: "Manchester" })}
+        >
           Manchester
         </Link>
-        <Link className="link-item" to={`/?query={"city":"Leeds"}`}>
+        <Link
+          className="link-item"
+          to={buildQueryString("query", { city: "Leeds" })}
+        >
           Leeds
         </Link>
-        <Link className="link-item" to={`/?query={"city":"Sheffield"}`}>
+        <Link
+          className="link-item"
+          to={buildQueryString("query", { city: "Sheffield" })}
+        >
           Sheffield
         </Link>
-        <Link className="link-item" to={`/?query={"city":"Liverpool"}`}>
+        <Link
+          className="link-item"
+          to={buildQueryString("query", { city: "Liverpool" })}
+        >
           Liverpool
+        </Link>
+        <p className="filter-title">Sort by:</p>
+        <Link
+          className="link-item"
+          to={buildQueryString("sort", { price: +1 })}
+        >
+          Price ascending
+        </Link>
+        <Link
+          className="link-item"
+          to={buildQueryString("sort", { price: -1 })}
+        >
+          Price descending
         </Link>
       </div>
     </div>
