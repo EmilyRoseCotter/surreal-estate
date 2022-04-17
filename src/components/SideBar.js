7f043ffda6a91e872a06702d95e89fa41fdf43cc
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-04-17 13:56:31 +0100

    added search component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import qs from "qs";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import "../styles/SideBar.css";
 
-const buildQueryString = (operation, valueObj) => {
-  const { search } = useLocation();
-
-  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
-  const newQueryParams = {
-    ...currentQueryParams,
-    [operation]: JSON.stringify(valueObj),
-  };
-  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
-};
-
 function SideBar() {
+  const [query, setQuery] = useState("");
+  const { search } = useLocation();
+  const history = useHistory();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    history.push(newQueryString);
+  };
+
   return (
     <div className="side-bar">
+      <form className="property-search" onSubmit={handleSearch}>
+        <input
+          className="property-search-input"
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button className="property-search-button" type="submit">
+          Search
+        </button>
+      </form>
       <p className="filter-title">Filter by city:</p>
       <div className="sidebar-links">
         <Link
